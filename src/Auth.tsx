9ab10669819a174a88477bf3c6537222a6ae3fcc
tangoyankee <miller.tim108@gmail.com>
2024-02-06 15:49:50 -0500

    login and logout flow for auth0

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -3,20 +3,23 @@ import { useEffect, useState } from "react";
 
 const issuer = new URL(process.env.REACT_APP_ISSUER_URL as string);
 const clientId = process.env.REACT_APP_CLIENT_ID as string;
-const clientSecret = process.env.REACT_APP_CLIENT_SECRET as string;
 const redirectUri = `${process.env.REACT_APP_HOST}/redirect`
 
 export function Auth () {
-    const [details, setDetails] = useState<{guid: string} | null>(null);
     const [loginPath, setLoginPath] = useState<string | null>(null);
+    const [userInfo, setUserInfo] = useState<oauth.UserInfoResponse | null>(null);
+    const [shouldSignOut, setShouldSignOut] = useState(false);
 
     useEffect(() => {
         (async () => {
-            const storedAccessToken = window.localStorage.getItem("testOidcAccessToken");
-            const storedSub = window.localStorage.getItem("testOidcSub");
+            const storedResultRaw = window.localStorage.getItem("testOidcResult");
+            const storedResult: oauth.OpenIDTokenEndpointResponse | null  = storedResultRaw === null ? storedResultRaw : JSON.parse(storedResultRaw);
+            const storedClaimsRaw = window.localStorage.getItem("testOidcClaims");
+            const storedClaims: oauth.IDToken | null = storedClaimsRaw === null ? storedClaimsRaw : JSON.parse(storedClaimsRaw);
             const storedCodeVerifier = window.localStorage.getItem("testOidcCodeVerifier");
             const codeVerifier = storedCodeVerifier !== null ? storedCodeVerifier :  oauth.generateRandomCodeVerifier();
             window.localStorage.setItem("testOidcCodeVerifier", codeVerifier);
+
             const as = await oauth.discoveryRequest(issuer).then((response) => oauth.processDiscoveryResponse(issuer, response))
             if (as.code_challenge_methods_supported?.includes('S256') !== true) {
                 throw new Error();
@@ -24,103 +27,114 @@ export function Auth () {
 
             const client: oauth.Client = {
                 client_id: clientId,
-                client_secret: clientSecret,
-                token_endpoint_auth_method: 'client_secret_basic'
+                token_endpoint_auth_method: 'none'
             }
 
-            const { pathname } = window.location;
-            if(storedAccessToken === null) {
+            if(shouldSignOut && storedResult !== null) {
+                console.debug("is signing out");
+                setShouldSignOut(false);
+                setUserInfo(null);
+                try{
+                    await oauth.revocationRequest(as, client, storedResult.access_token);
+                    window.localStorage.removeItem("testOidcResult");
+                    window.localStorage.removeItem("testOidcClaims");
+                    window.localStorage.removeItem("testOidcCodeVerifier");
 
-                const codeChallenge = await oauth.calculatePKCECodeChallenge(codeVerifier);
-                const codeChallengeMethod ='S256';
-                window.localStorage.setItem("testOidcCodeVerifier", codeVerifier);
-                const authUrl = new URL(as.authorization_endpoint!);
+                    window.location.assign('/');
+                } catch (e){
+                    console.error("revocation error", e)
+                }
+            } else {
+                const { pathname } = window.location;
+                if(storedResult === null && loginPath === null) {
+                    console.debug("logged out and needs login path");
 
-                authUrl.searchParams.set('client_id', client.client_id);
-                authUrl.searchParams.set('code_challenge', codeChallenge);
-                authUrl.searchParams.set('code_challenge_method', codeChallengeMethod);
-                authUrl.searchParams.set('redirect_uri', redirectUri);
-                authUrl.searchParams.set('response_type', 'code');
-                authUrl.searchParams.set('scope', 'openid email');
-                setLoginPath(authUrl.href);
-            }
+                    const codeChallenge = await oauth.calculatePKCECodeChallenge(codeVerifier);
+                    const codeChallengeMethod ='S256';
+                    window.localStorage.setItem("testOidcCodeVerifier", codeVerifier);
+                    const authUrl = new URL(as.authorization_endpoint!);
 
+                    authUrl.searchParams.set('client_id', client.client_id);
+                    authUrl.searchParams.set('code_challenge', codeChallenge);
+                    authUrl.searchParams.set('code_challenge_method', codeChallengeMethod);
+                    authUrl.searchParams.set('redirect_uri', redirectUri);
+                    authUrl.searchParams.set('response_type', 'code');
+                    authUrl.searchParams.set('scope', 'openid email');
+                    setLoginPath(authUrl.href);
+                }
 
-            if (pathname.includes('redirect') && storedAccessToken === null) {
-                const currentUrl = new URL(window.location.href);
-                const params = oauth.validateAuthResponse(as, client, currentUrl, oauth.expectNoState)
-                if (oauth.isOAuth2Error(params)) throw new Error("OAuth 2.0")
+                if (pathname.includes('redirect') && storedResult === null) {
+                    console.info("is logged out but has access to auth code that could log them in");
 
-                const response = await oauth.authorizationCodeGrantRequest(
-                    as,
-                    client,
-                    params,
-                    redirectUri,
-                    codeVerifier,
-                )
+                    const currentUrl = new URL(window.location.href);
+                    const params = oauth.validateAuthResponse(as, client, currentUrl, oauth.expectNoState)
+                    if (oauth.isOAuth2Error(params)) {
+                        console.error("params error");
+                    } else {
+                        const response = await oauth.authorizationCodeGrantRequest(
+                            as,
+                            client,
+                            params,
+                            redirectUri,
+                            codeVerifier,
+                        )
 
-                const challenges = oauth.parseWwwAuthenticateChallenges(response);
-                if(challenges !== undefined) {
-                    for (const challenge of challenges) {
-                        console.info('challenge', challenge);
+                        const challenges = oauth.parseWwwAuthenticateChallenges(response);
+                        if(challenges !== undefined) {
+                            for (const challenge of challenges) {
+                                console.info('challenge', challenge);
+                            }
+
+                            throw new Error();
+                        }
+
+                        const result = await oauth.processAuthorizationCodeOpenIDResponse(as, client, response);
+                        if(oauth.isOAuth2Error(result)) {
+                            console.error('error', result);
+                        } else {
+                            window.localStorage.setItem("testOidcResult", JSON.stringify(result));
+                            const claims  = oauth.getValidatedIdTokenClaims(result);
+                            window.localStorage.setItem("testOidcClaims", JSON.stringify(claims));
+                            setLoginPath(null);
+                            window.location.assign('/');
+                        }
+                    }
+                }
+
+                if(storedResult !== null && storedClaims !== null && userInfo === null) {
+                    console.info("has access to tokens but needs to place user info in state")
+
+                    const userInfoResponse = await oauth.userInfoRequest(as, client, storedResult.access_token);
+
+                    const userChallenges = oauth.parseWwwAuthenticateChallenges(userInfoResponse);
+                    if (userChallenges !== undefined) {
+                        for (const challenge of userChallenges) {
+                        console.info('challenge', challenge)
+                        }
+                        throw new Error() // Handle www-authenticate challenges as needed
                     }
 
-                    throw new Error();
+                    const userInfoResult = await oauth.processUserInfoResponse(as, client, storedClaims.sub, userInfoResponse);
+                    setUserInfo(userInfoResult)
                 }
-
-                const result = await oauth.processAuthorizationCodeOpenIDResponse(as, client, response);
-                if(oauth.isOAuth2Error(result)) {
-                    console.error('error', result);
-                    throw new Error();
-                }
-
-                const { access_token: accessToken } = result;
-                const claims = oauth.getValidatedIdTokenClaims(result);
-                const { sub } = claims;
-                window.localStorage.setItem("testOidcAccessToken", accessToken);
-                window.localStorage.setItem("testOidcSub", sub);
-            }
-            if(storedAccessToken !== null && storedSub !== null) {
-
-                const userInfoResponse = await oauth.userInfoRequest(as, client, storedAccessToken);
-
-                const userChallenges = oauth.parseWwwAuthenticateChallenges(userInfoResponse);
-                if (userChallenges !== undefined) {
-                    for (const challenge of userChallenges) {
-                    console.info('challenge', challenge)
-                    }
-                    throw new Error() // Handle www-authenticate challenges as needed
-                }
-
-                const userInfoResult = await oauth.processUserInfoResponse(as, client, storedSub, userInfoResponse);
-                console.debug(userInfoResult)
-                await oauth.revocationRequest(as, client, storedAccessToken);
-                const revokedUserInfoResponse = await oauth.userInfoRequest(as, client, storedAccessToken);
-                console.debug('revoked', revokedUserInfoResponse);
-
-                window.localStorage.removeItem("testOidcAccessToken");
-                window.localStorage.removeItem("testOidcSub");
-                window.location.assign('/');
             }
         })();
-    }, [])
+    }, [loginPath, userInfo, shouldSignOut])
 
-    const signIn = () => setDetails({ guid: "12345"});
-    const signOut = () => setDetails(null);
+    const signOut = () => setShouldSignOut(true);
     return <div>
         <h1>
             Auth
         </h1>
         <div>
             {
-                details === null ?
+                userInfo === null ?
                 <>
-                    <button onClick={signIn}>Sign In</button>
-                    {loginPath !== null && <a href={loginPath}>login</a>} 
+                    {loginPath !== null && <a href={loginPath}>Login</a>} 
                 </> :
                 <>
                     <button onClick={signOut}>Sign Out</button>
-                    <p>{details.guid}</p>
+                    <p>{userInfo.email}</p>
                 </>
             }
         </div>
