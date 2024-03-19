95efeeaf0c95fb296664dd2d4613c714848efd18
tangoyankee <miller.tim108@gmail.com>
2024-03-19 17:34:07 -0400

    attempt to get site details with access token

--- server/app.ts
+++ server/app.ts
@@ -1,35 +1,45 @@
 import express from "express";
 import cors from "cors";
+import msal from "@azure/msal-node";
 import "dotenv/config";
-import jsonwebtoken from "jsonwebtoken";
-import jwkToPem from "jwk-to-pem";
-
-const jwtVerifyEndpoint =`${process.env.ISSUER_URL}/.well-known/jwks.json`
 
 const app = express();
 const port = 5500;
 
 app.use(cors());
 
-app.get('/', async (req: express.Request, res: express.Response) => {
+app.get('/', async (_req: express.Request, res: express.Response) => {
+    const clientId = process.env.SHAREPOINT_CLIENT_ID;
+    const clientSecret = process.env.SHAREPOINT_CLIENT_SECRET;
+    const tenantId = process.env.SHAREPOINT_TENANT_ID;
+    if(clientId === undefined || clientSecret === undefined || tenantId === undefined) throw new Error("Missing configuration")
+    const msalConfig = {
+       auth: {
+        clientId,
+        clientSecret,
+        authority: `https://login.microsoftonline.com/${tenantId}`
+       } 
+    }
+    const cca = new msal.ConfidentialClientApplication(msalConfig);
+    const result = await cca.acquireTokenByClientCredential({
+        scopes: ["https://graph.microsoft.com/.default"]
+    })
+    // console.debug("result", result);
+    // console.debug("clientId", clientId);
+    // console.debug("clientSecret", clientSecret);
+    // console.debug("tenantId", tenantId);
+    if(result === null) throw new Error("token result is null");
+    const { accessToken } = result;
+    const url = `https://graph.microsoft.com/v1.0/sites/${process.env.SHAREPOINT_TARGET_HOST}`
+    const options = {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            Accept: 'application/json'
+        }
+    }
+    const siteDetails = await fetch(url, options);
+    console.debug("siteDetails", siteDetails);
     try {
-        const response  = await fetch(jwtVerifyEndpoint);
-        const content = await response.json();
-
-        // @ts-ignore
-        const key  = content.keys[0];
-        
-        const headers = req.headers;
-        const authorization = headers.authorization;
-        const [_, accessToken ] = authorization !== undefined ? authorization?.split(" ") : [];
-        
-        const verify = jsonwebtoken.verify(accessToken, jwkToPem(key), {
-            algorithms: [key.alg],
-            audience: `http://localhost:${port}`
-        });
-        
-        console.info(verify);
-
         const message = {
             status: "verified"
         }
